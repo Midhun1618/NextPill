8788d04be273872f548a92d67babe800b2b363d8
Midhun1618 <midhumidhun342@gmail.com>
2025-11-17 14:01:22 +0530

    dialog box added for newMed

--- app/src/main/java/com/voxcom/nextpill/MainActivity.kt
+++ app/src/main/java/com/voxcom/nextpill/MainActivity.kt
@@ -1,20 +1,44 @@
 package com.voxcom.nextpill
 
+import android.app.Dialog
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    lateinit var addNewBtn : Button
+    lateinit var editListBtn : Button
+    lateinit var userName: TextView
+    lateinit var userAge : TextView
+    lateinit var camBtn : Button
+    lateinit var fileBtn : Button
+    lateinit var customBtn : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
+        addNewBtn = findViewById(R.id.addNew)
+
+        val dialog = Dialog(this)
+        dialog.setContentView(R.layout.addnew_dialog)
+        dialog.setCancelable(true)
+
+
+
+
+        addNewBtn.setOnClickListener{
+            dialog.show()
+        }
+
     }
 }
